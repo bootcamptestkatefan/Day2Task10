adec3132e5241577301dffc787efc97adfcecbed
bootcamptestkatefan <kate.fan@oocl.com>
2018-11-21 00:37:37 +0800

    finish 5 function

--- __Tests__/createMultibleTable-spec.js
+++ __Tests__/createMultibleTable-spec.js
@@ -15,3 +15,12 @@ it ('check Number Smaller Than One Thousand Inclusively', () => {
     expect(checkNumberSmallerThanOneThousandInclusively(4,1001)).toBe(false);
 });
 
+const checkInputIsIntegerOrNot = require('../createMultibleTable.js').checkInputIsIntegerOrNot;
+it ('check Input Is Integer Or Not', () => {
+    expect(checkInputIsIntegerOrNot(4,2)).toBe(true);
+});
+
+const calculateTheResultForTwoInputs = require('../createMultibleTable.js').calculateTheResultForTwoInputs;
+it ('calculate The Result For Two Inputs', () => {
+    expect(calculateTheResultForTwoInputs(2,4)).toBe("2*4=8");
+});
--- createMultibleTable.js
+++ createMultibleTable.js
@@ -12,7 +12,15 @@ function checkNumberSmallerThanOneThousandInclusively (a,b){
     return a<=1000&&b<=1000;
 }
 
+function checkInputIsIntegerOrNot (a,b){
+    return Number.isInteger(a)&&Number.isInteger(b);
+} 
+
+function calculateTheResultForTwoInputs (a,b){
+    return a + '*' + b + '=' + a*b;
+}
 module.exports = {checkFirstSmallerThanLast,
                   checkNumberLargerThanOneInclusively,
                   checkNumberSmallerThanOneThousandInclusively,
-                  };
+                  checkInputIsIntegerOrNot,
+                  calculateTheResultForTwoInputs};
